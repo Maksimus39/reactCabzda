2f2857606b89bf56a839d5c68e9fdcf3ecaa15dd
Maksim <maksim.minakov.83@mail.ru>
2023-04-25 14:53:59 +0300

    01Poigral_s_Peremennimi

--- src/index.tsx
+++ src/index.tsx
@@ -21,3 +21,61 @@ reportWebVitals();
 
 
 
+let a = 10;
+a = 12;
+
+//a = "Macson";  // в Ts нельзя переназначать данные с чисел на строки,будет предупреждение
+
+let b = {name: "Macsim"};
+//b= 12;
+//b = "Macsim"; // мы можем к ранее объявленному объекту(если он пустой) приклеить и цифру и строку.
+
+let c = {name: "Larisa",
+                      age: 37,
+
+};
+c = {name: "Olga",
+     age: 50,
+};                    // а переназначать объекты мы можем
+
+
+
+let d = {name: "Larisa",
+    age: 37,
+    address: {
+    city:  "Lipetsk",
+    country: "Russia"
+    }
+};
+console.log(d.address.country)
+
+let users = [
+    {
+        name: "Max",
+        age: 39,
+        address: {
+            city: "Lipeck",
+            country: "Russia"
+        }
+    },
+    {
+        name: "Larisa",
+        age: 39,
+        address: {
+            city: "Lipeck",
+            country: "Russia"
+        }
+    }
+]
+console.log(users[1].address.city)
+
+
+
+
+
+
+
+
+
+
+
